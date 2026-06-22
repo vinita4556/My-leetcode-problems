70a789bbcb02834aee40b52619324fcb27525a59
Vinita Parmar <vinitaparamar@gmail.com>
2026-06-22 09:26:29 +0530

    Time: 1 ms (57.68%), Space: 8.5 MB (92.2%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int original = x;
+        long long reverse = 0;
+
+        while(x > 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+};

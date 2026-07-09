12c762b2d88edc153beff312c32c13e58b14770b
Vinita Parmar <vinitaparamar@gmail.com>
2026-07-09 16:18:16 +0530

    Time: 41 ms (25.84%), Space: 14.1 MB (81.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Brute force approach
         for(int i = 0; i<nums.size(); i++){
             for(int j = i+1; j<nums.size(); j++){
                 if( nums[i] + nums[j] == target )
